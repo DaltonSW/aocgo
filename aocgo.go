3d6a69229100263d41bc9072d14ae854296da327
Dalton Williams <daltonstwilliams@gmail.com>
2024-08-02 14:21:26 -0500

    Renaming of main files and adding some documentation

--- aocgo.go
+++ aocgo.go
@@ -1,3 +1,4 @@
+// Package aocgo provides data access functions for a non-intrusive way to get your Advent of Code puzzle inputs
 package aocgo
 
 import (
@@ -9,6 +10,7 @@ import (
 	"github.com/charmbracelet/log"
 )
 
+// GetInputAsByteArray will return the user's puzzle input, as determined by the file's working directory, as an array of bytes.
 func GetInputAsByteArray() []byte {
 	year, day, err := dirparse.GetYearAndDayFromCWD()
 	if err != nil {
@@ -18,10 +20,12 @@ func GetInputAsByteArray() []byte {
 	return getData(year, day)
 }
 
+// GetInputAsString will return the user's puzzle input, as determined by the file's working directory, as a single string.
 func GetInputAsString() string {
 	return string(GetInputAsByteArray())
 }
 
+// GetInputAsLineArray will return the user's puzzle input, as determined by the file's working directory, as an array of strings, split on newline.
 func GetInputAsLineArray() []string {
 	return strings.Split(GetInputAsString(), "\n")
 }
---
+++ aocutils/aocutils.go
@@ -0,0 +1,3 @@
+// Package aocutils is a collection of commonly used functions that might be helpful for Advent of Code solving.
+// User puzzle input can be obtained without importing this sub-package, if you wish to handle ALL of the logic code yourself.
+package aocutils
