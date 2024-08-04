502416fc480e247c957e6b9fe3dbfc82f6fdb0e3
Dalton Williams <daltonstwilliams@gmail.com>
2024-08-04 09:28:32 -0500

    Using my own help keys to provide a help to the viewport

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -29,16 +31,66 @@ var (
 	}()
 )
 
+type keymap struct {
+	Up     key.Binding
+	Down   key.Binding
+	Submit key.Binding
+	Input  key.Binding
+	Quit   key.Binding
+}
+
+func (k keymap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Up, k.Down, k.Quit, k.Input, k.Submit}
+}
+
+func (k keymap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down, k.Quit},
+		{k.Input, k.Submit},
+	}
+}
+
+var keys = keymap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("↑/k", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("↓/j", "move down"),
+	),
+	Submit: key.NewBinding(
+		key.WithKeys("s"),
+		key.WithHelp("s", "submit answer"),
+	),
+	Input: key.NewBinding(
+		key.WithKeys("i"),
+		key.WithHelp("i", "download input"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q/esc", "quit"),
+	),
+}
+
 type model struct {
 	content   string
 	ready     bool
+	keys      keymap
 	viewport  viewport.Model
+	help      help.Model
 	viewTitle string
 }
 
 func StartViewportWithArr(input []string, title string) {
 	contentStr := strings.Join(input, "")
-	p := tea.NewProgram(model{content: contentStr, viewTitle: title}, tea.WithAltScreen(), tea.WithMouseCellMotion())
+	m := model{
+		content:   contentStr,
+		viewTitle: title,
+		keys:      keys,
+		help:      help.New(),
+	}
+	p := tea.NewProgram(m, tea.WithAltScreen(), tea.WithMouseCellMotion())
 
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Couldn't run viewport:", err)
@@ -48,20 +100,9 @@ func StartViewportWithArr(input []string, title string) {
 
 // TODO: Add commands for submitting and downloading input
 
-func StartViewport(input string, title string) {
-	p := tea.NewProgram(model{content: input, viewTitle: title}, tea.WithAltScreen(), tea.WithMouseCellMotion())
-
-	if _, err := p.Run(); err != nil {
-		fmt.Println("Couldn't run viewport:", err)
-		os.Exit(1)
-	}
-}
-
 func (m model) Init() tea.Cmd {
 	return nil
 }
-
-// BUG: Always reports 100% scroll, and doesn't actually let you scroll
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var (
@@ -106,8 +147,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	// Handle keyboard and mouse events in the viewport
 	m.viewport, cmd = m.viewport.Update(msg)
 	cmds = append(cmds, cmd)
-	// log.Debugf("Viewport Height: %d\n", m.viewport.Height)
-	// log.Debugf("Content Height: %d\n", lipgloss.Height(m.content))
 
 	return m, tea.Batch(cmds...)
 }
@@ -129,5 +168,5 @@ func (m model) headerView() string {
 func (m model) footerView() string {
 	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
 	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+	return lipgloss.JoinHorizontal(lipgloss.Center, line, info) + "\n" + lipgloss.JoinHorizontal(lipgloss.Center, m.help.View(m.keys))
 }
